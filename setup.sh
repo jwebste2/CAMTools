2cfdfe8666566564cfa6b554a6d02d49c3bcf160
jwebste2 <jwebste2@gmail.com>
2017-03-23 09:59:15 -0500

    Adding Latex documentation

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export NSPROOT=/users/jwebster/L1Track/Cavium/NSP-SDK-2.2.1
+cd /users/jwebster/OCTEON-SDK
+source env-setup OCTEON_CN68XX_PASS2_1
+
+#pushd /usr/local/Cavium_Networks/OCTEON-SDK
+#source env-setup OCTEON_CN68XX_PASS2_1
+#popd
+
